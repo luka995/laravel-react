07bb3f83344628ebcdcbf94f8f7741d054062bfc
Luka Stosic <luka@yandex.com>
2024-01-16 16:27:35 +0100

    react router - layouts and context provider for storing user and token state

---
+++ react/src/components/DefaultLayout.jsx
@@ -0,0 +1,16 @@
+import {Navigate, Outlet} from "react-router-dom";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
+
+export default function DefaultLayout() {
+    const {user, token} = useStateContext();
+
+    if (!token) {
+        return <Navigate to="/login"/>
+    }
+
+    return (
+        <div>
+            <Outlet/>
+        </div>
+    )
+}
