cb3501bdaba8fb22a397149bc28efff782d3c9f5
Luka Stosic <luka@yandex.com>
2024-02-28 11:49:33 +0100

    small react users view code for state

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,4 +1,28 @@
+import {useEffect, useState} from "react";
+import axiosClient from "../axios-client.js";
+
 export default function Users() {
+
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+        getUsers();
+    }, []);
+
+    const getUsers = () => {
+        setLoading(true);
+        axiosClient.get('/users')
+            .then(({data}) => {
+                //todo users set
+                setLoading(false);
+                console.log(data);
+            })
+            .catch(() => {
+                setLoading(false);
+            })
+    };
+
     return (
         <div>
             Users
